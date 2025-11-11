3b8a9cf980c5d92118bc87e28470618bd3f31898
GAURI CHANDRASHEKHAR DANDEKAR <gauridandekar25@gmail.com>
2025-11-11 08:56:41 +0530

    Add files via upload

---
+++ program for  even series.php
@@ -0,0 +1,9 @@
+<?php
+echo "Even series from 1 to 10\n";
+$i = 2;    // Initialize the variable before the loop
+while ($i <= 10)
+{
+echo $i . "\n";
+$i += 2;   // Increment by 2 to get even numbers
+}
+?>
