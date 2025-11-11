1b8804c13bfb9a0537715ef5ef0fa44124027722
GAURI CHANDRASHEKHAR DANDEKAR <gauridandekar25@gmail.com>
2025-11-11 09:35:47 +0530

    Update simple calculator.php

--- simple calculator.php
+++ simple calculator.php
@@ -1,23 +1,38 @@
 <?php
-$a = 10;
-$b = 5;
-$operator = '*';
+echo "Simple PHP Calculator\n";
+
+echo "Enter first number: ";
+$num1 = trim(fgets(STDIN));
+
+echo "Enter operator (+, -, *, /): ";
+$operator = trim(fgets(STDIN));
+
+echo "Enter second number: ";
+$num2 = trim(fgets(STDIN));
+
+$result = 0;
 
 switch ($operator) {
     case '+':
-        echo "Result: " . ($a + $b);
+        $result = $num1 + $num2;
         break;
     case '-':
-        echo "Result: " . ($a - $b);
+        $result = $num1 - $num2;
         break;
     case '*':
-        echo "Result: " . ($a * $b);
+        $result = $num1 * $num2;
         break;
     case '/':
-        echo "Result: " . ($a / $b);
+        if ($num2 == 0) {
+            echo "Error: Division by zero is not allowed.\n";
+            exit;
+        }
+        $result = $num1 / $num2;
         break;
     default:
-        echo "Invalid Operator";
+        echo "Invalid operator!\n";
+        exit;
 }
-echo "<br>";
+
+echo "Result: $num1 $operator $num2 = $result\n";
 ?>
