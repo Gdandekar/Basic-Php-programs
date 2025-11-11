db3d2b2e7cc8fa4d6b922336758fb7908b888595
GAURI CHANDRASHEKHAR DANDEKAR <gauridandekar25@gmail.com>
2025-11-11 09:30:01 +0530

    Update fibonacii progeam.php

--- fibonacii progeam.php
+++ fibonacii progeam.php
@@ -1,18 +1,25 @@
 <?php
-echo "Enter number of terms: ";
-$n = trim(fgets(STDIN));  // Reads input from user and removes newline
+if (isset($_POST['n'])) {
+    $n = (int)$_POST['n'];
 
-$firstterm = 0;
-$secondterm = 1;
+    $firstterm = 0;
+    $secondterm = 1;
 
-echo "Fibonacci series up to $n terms:\n";
+    echo "<h3>Fibonacci series up to $n terms:</h3>";
 
-for ($i = 0; $i < $n; $i++) {
-    echo $firstterm . " ";
-    $nextterm = $firstterm + $secondterm;
-    $firstterm = $secondterm;
-    $secondterm = $nextterm;
+    for ($i = 0; $i < $n; $i++) {
+        echo $firstterm . " ";
+        $nextterm = $firstterm + $secondterm;
+        $firstterm = $secondterm;
+        $secondterm = $nextterm;
+    }
+} else {
+?>
+    <form method="post">
+        <label>Enter number of terms:</label>
+        <input type="number" name="n" min="1" required>
+        <input type="submit" value="Generate">
+    </form>
+<?php
 }
-
-echo "\n";
-?>  
+?>
